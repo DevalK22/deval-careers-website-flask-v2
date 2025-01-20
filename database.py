faa86171050cb5e24b6188f89f88dc1b9a98993f
DevalK22 <145742283+DevalK22@users.noreply.github.com>
2025-01-20 17:14:09 +0000

    Made website functional with buttons

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
+from flask import jsonify
 from sqlalchemy import create_engine, text
 import os
 
 db_conn_str = os.environ['DB_CONNECTION_STRING']
 engine = create_engine(db_conn_str)
+
 
 def load_jobs_from_db():
     with engine.connect() as connection:
@@ -12,3 +14,16 @@ def load_jobs_from_db():
         for row in result.all():
             jobs.append(row._asdict())
         return jobs
+
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text('SELECT * from jobs where id = :val'),
+                              {"val": id})
+        job = []
+        for row in result.all():
+            job.append(row._asdict())
+            if len(row) == 0:
+                return None
+            else:
+                return dict(job[0])
